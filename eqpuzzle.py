c2fbca5bce29bdc1ccf755371563a0091ef3f355
dwunderl <Ted@0995>
2023-01-19 18:50:32 -0700

    Enhanced with classes, loading function, etc

--- eqpuzzle.py
+++ eqpuzzle.py
@@ -1,33 +1,70 @@
 # import PySimpleGUI as sg
 # sg.Window(title="Hello World", layout=[[]], margins=(100, 50)).read()
 
+# A class for the 4 numbers and goal - eqParams
+# Class level object with an array of 4 numbers
+# A class for a permutation, expression and result - eqCandidate
 
+import csv
+from itertools import permutations,product
+
+class eqCandidate:
+    def __init__(self,expression, result):
+        self.expression = expression
+        self.result = result
+    
+    def print_info(self):
+        print(f"Expression: {self.expression}, Result: {self.result}")
+
+class eqParams:
+    def __init__(self,numbers, goal):
+        self.numbers = numbers
+        self.goal = goal
+    def __init__(self):
+        # Initialize class attributes here
+        self.numbers = [0] * 4
+        self.goal = 0
+    def print_info(self):
+        print(f"Numbers: {self.numbers}, Goal: {self.goal}")
+
+# generate expressions for each number permutation against operation permutations
+# and then all parenthesized orderings
+# and then evaluate the expression to calculate the result
+
+def evaluateExpression(expression, goal, solutions):
+    try:
+        result = eval(expression)
+    except ZeroDivisionError:
+        print("You can't divide by zero!")
+        result = -999
+    finally:
+        #print (expression + " == " + str(result))
+        if result == goal:
+            solution = eqCandidate(expression, result)
+            solution.print_info()
+            solutions.append(solution)
 
 msg = "Hello World"
 print(msg)
 
-import csv
+def getParamsFromCsv(csvFileName):
+    eqparams = eqParams()
 
-goal = 0
-numberArray = []
-with open('eqpuzzle.csv', 'r') as file:
-    reader = csv.reader(file)
-    for row in reader:
-        print(row)
-        numberArray = [row[0], row[1], row[2], row[3]]
-        goal = int(row[4])
+    with open(csvFileName, 'r') as file:
+        reader = csv.reader(file)
+        for row in reader:
+            print(row)
+            eqparams.numbers = [row[0], row[1], row[2], row[3]]
+            eqparams.goal = int(row[4])
+    return eqparams
 
-
-from itertools import permutations,product
+eqparams = getParamsFromCsv("eqpuzzle.csv")
 
 # Numbers Permutation generation
 
-#goal = 19
-#numberArray = ["22", "12", "9", "7"]
-
 numberPermutations = []
 uniqueNumberPermutations = []
-for numberPermutation in list(permutations(numberArray,4)):
+for numberPermutation in list(permutations(eqparams.numbers,4)):
     numberPermutations.append(numberPermutation)
 
     # Only keep isUnique permutations
@@ -46,30 +83,7 @@ operationPermutations = product(operations, repeat=3)
 #     print(operationPermutation)
 
 
-# generate expressions for each number permutation against operation permutations
-# and then all parenthesized orderings
-# and then evaluate the expression to calculate the result
 
-class Solution:
-    def __init__(self,expression, result):
-        self.expression = expression
-        self.result = result
-    
-    def print_info(self):
-        print(f"Expression: {self.expression}, Result: {self.result}")
-
-def evaluateExpression(expression, goal, solutions):
-    try:
-        result = eval(expression)
-    except ZeroDivisionError:
-        print("You can't divide by zero!")
-        result = -999
-    finally:
-        #print (expression + " == " + str(result))
-        if result == goal:
-            solution = Solution(expression, result)
-            solution.print_info()
-            solutions.append(solution)
 
 solutions = []
 for np in uniqueNumberPermutations:
@@ -79,11 +93,11 @@ for np in uniqueNumberPermutations:
     for op in operationPermutations:
         #print(op)
         expression = str("(" + np[0] + op[0] + "(" + np[1] + op[1] + np[2] + "))" + op[2] + np[3])
-        evaluateExpression(expression, goal, solutions)
+        evaluateExpression(expression, eqparams.goal, solutions)
         expression = str(np[0] + op[0] + "((" + np[1] + op[1] + np[2] + ")" + op[2] + np[3] +")")
-        evaluateExpression(expression, goal, solutions)
+        evaluateExpression(expression, eqparams.goal, solutions)
         expression = str(np[0] + op[0] + "(" + np[1] + op[1] + "(" + np[2] + op[2] + np[3] +"))")
-        evaluateExpression(expression, goal, solutions)
+        evaluateExpression(expression, eqparams.goal, solutions)
         expression = str("(" + np[0] + op[0] + np[1] + ")" + op[1] + "(" + np[2] + op[2] + np[3] +")")
-        evaluateExpression(expression, goal, solutions)
+        evaluateExpression(expression, eqparams.goal, solutions)
 
